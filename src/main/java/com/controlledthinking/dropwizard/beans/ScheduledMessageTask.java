08e60b4fa5493641ea8723b940b6825ab770c5f5
Bradley Rintoul <bradleyrintoul@bradleys-mbp.attlocal.net>
2017-06-23 23:35:56 -0700

    Starting to add testing.

--- src/main/java/com/controlledthinking/dropwizard/beans/ScheduledMessageTask.java
+++ src/main/java/com/controlledthinking/dropwizard/beans/ScheduledMessageTask.java
@@ -6,6 +6,9 @@
 package com.controlledthinking.dropwizard.beans;
 
 import io.dropwizard.lifecycle.Managed;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +18,16 @@ import org.slf4j.LoggerFactory;
  */
 public class ScheduledMessageTask implements Managed {
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final ScheduledExecutorService scheduler;
+
+    public ScheduledMessageTask() {
+        this.scheduler = Executors.newScheduledThreadPool(1);
+    }
 
     @Override
     public void start() throws Exception {
-        log.info("We are starting the timer task to check for new scheduled messages.");
+        log.info("We are starting the timer task to check for new scheduled messages."); 
+        scheduler.scheduleAtFixedRate(() -> { System.out.println("WOW!"); }, 8, 8, TimeUnit.SECONDS);
     }
 
     @Override
---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Test;
+
+/**
+ *
+ * @author bradleyrintoul
+ */
+public class CustomerTest {
+    @Test
+    public void doIt() {
+        System.out.println("DO IT!!!");
+    }
+}
